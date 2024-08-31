068314eefde31002855c4ce19ee1fa017cdf8662
salmanshahriar <salmanshahriar16@gmail.com>
2024-08-31 14:22:47 +0600

    updated

--- app/dashboard/[id]/page.jsx
+++ app/dashboard/[id]/page.jsx
@@ -71,7 +71,7 @@ const AiAnalyzeCard = ({ title, percentage, alertMessage }) => {
 
 const Profile = () => {
   return (
-    <div className="mx-auto pb-2 pt-1 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 min-h-screen flex flex-col justify-start bg-gradient-to-br from-gray-700 via-indigo-900 to-gray-600">
+    <div className="mx-auto p-3 pt-1 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 min-h-screen flex flex-col justify-start bg-gradient-to-br from-gray-700 via-indigo-900 to-gray-600">
       <h1 className="text-center text-md md:text-3xl text-white mb-10">
         Dashboard-1 / <span className="text-white/50">Device-1</span>
       </h1>
