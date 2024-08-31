d4c305a09c6aa0b0cff8c48bac43497a88412deb
salmanshahriar <salmanshahriar16@gmail.com>
2024-08-31 06:13:52 +0600

    updated

--- app/dashboard/[id]/page.jsx
+++ app/dashboard/[id]/page.jsx
@@ -49,7 +49,7 @@ const CircularProgressBar = ({ percentage }) => {
 
 const VisualizationCard = ({ title, percentage }) => {
   return (
-    <div className="bg-white/10 backdrop-blur-md pt-1 pb-3 rounded-lg shadow-lg flex flex-col items-center">
+    <div className="bg-white/10 backdrop-blur-md pb-3 rounded-lg shadow-lg flex flex-col items-center">
       <CircularProgressBar percentage={percentage} />
       <h2 className="text-sm font-semibold text-white mb-2">{title}</h2>
     </div>
@@ -71,7 +71,7 @@ const AiAnalyzeCard = ({ title, percentage, alertMessage }) => {
 
 const Profile = () => {
   return (
-    <div className="mx-auto p-4 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 min-h-screen flex flex-col justify-start bg-gradient-to-br from-gray-700 via-indigo-900 to-gray-600">
+    <div className="mx-auto pb-2 pt-1 sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 min-h-screen flex flex-col justify-start bg-gradient-to-br from-gray-700 via-indigo-900 to-gray-600">
       <h1 className="text-center text-md md:text-3xl text-white mb-10">
         Dashboard-1 / <span className="text-white/50">Device-1</span>
       </h1>
